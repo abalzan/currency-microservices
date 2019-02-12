35aef8bed10c13819a5434b7394edfe1800b2d6c
abalzan <andreibalzan@gmail.com>
2019-02-12 20:50:22 +0000

    configuring ribbon load balance

--- currency-conversion-service/src/main/java/com/andrei/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/andrei/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,10 +1,12 @@
 package com.andrei.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "${currency-exchange-service.url}")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 
