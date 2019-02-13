152378cea15eda2a0ec14cc49c8582ff8dcf1c71
abalzan <andreibalzan@gmail.com>
2019-02-13 06:55:10 +0000

    first commit

---
+++ netflix-zull-api-gateway-server/src/main/java/com/andrei/microservices/netflixzullapigatewayserver/NetflixZullApiGatewayServerApplication.java
@@ -0,0 +1,14 @@
+package com.andrei.microservices.netflixzullapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZullApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZullApiGatewayServerApplication.class, args);
+	}
+
+}
+
