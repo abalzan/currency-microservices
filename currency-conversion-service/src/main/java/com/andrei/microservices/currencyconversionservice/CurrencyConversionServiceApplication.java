e253bd78ba6033886f29b3bee80cde5d8ce14136
abalzan <andreibalzan@gmail.com>
2019-02-13 06:14:16 +0000

    configuring Eureka client for currency-conversion-service

--- currency-conversion-service/src/main/java/com/andrei/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/andrei/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.andrei.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients("com.andrei.microservices.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
