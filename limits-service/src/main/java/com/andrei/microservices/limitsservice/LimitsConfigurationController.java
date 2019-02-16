2b9a1646b2d44c843b9ebf99615ff95294faed24
abalzan <andreibalzan@gmail.com>
2019-02-16 15:32:48 +0000

    configuring fault tolerance with Hystrix

--- limits-service/src/main/java/com/andrei/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/andrei/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.andrei.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration(){
         return new LimitConfiguration(limitConfiguration.getMinimum(), limitConfiguration.getMaximum());
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    @GetMapping("/fault-tolerance-sample")
+    public LimitConfiguration faultTolerace(){
+        throw new RuntimeException("EXCEPTION");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration(){
+        return new LimitConfiguration(9, 9876);
     }
 }
--- limits-service/src/main/java/com/andrei/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/andrei/microservices/limitsservice/LimitsServiceApplication.java
@@ -4,8 +4,10 @@ import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
+@EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
 public class LimitsServiceApplication {
